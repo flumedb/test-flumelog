d788bb986eeeea432dee179f309d891536cc6be4
Dominic Tarr <dominic.tarr@gmail.com>
2019-03-23 22:40:38 +1300

    make sure that the offset of the first item written is not -1 (it may be 0)

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
 
 var pull = require('pull-stream')
-//var tape = require('tape')
+var tape = require('tape')
 var Spec = require('pull-spec')
 var assert = require('assert')
 
-
-function FastForward () {
-  var tests = [], started = false
-  function next () {
-    var called = false
-    assert.end = function () {
-      if(called) throw new Error('end called twice in:'+name)
-      called = true
-      next()
-    }
-    if(tests.length) {
-      var fn = tests.shift()
-      console.log('# '+fn._name)
-      fn(assert)
-    }
-    else
-      started = false
-  }
-  return function tape (name, fn) {
-    if(!fn) next()
-    else {
-      fn._name = name
-      tests.push(fn)
-    }
-    if(!started) {
-      started = true
-      next()
-    }
-  }
-}
+//I don't recognise this code at all!
+//function FastForward () {
+//  var tests = [], started = false
+//  function next () {
+//    var called = false
+//    assert.end = function () {
+//      if(called) throw new Error('end called twice in:'+name)
+//      called = true
+//      next()
+//    }
+//    if(tests.length) {
+//      var fn = tests.shift()
+//      console.log('# '+fn._name)
+//      fn(assert)
+//    }
+//    else
+//      started = false
+//  }
+//  return function tape (name, fn) {
+//    if(!fn) next()
+//    else {
+//      fn._name = name
+//      tests.push(fn)
+//    }
+//    if(!started) {
+//      started = true
+//      next()
+//    }
+//  }
+//}
 
 module.exports = function (createLog, cb) {
-  var tape = FastForward()
-  var log = createLog()
+  var filename = '/tmp/test-flumelog_'+Date.now()
+//  var tape = FastForward()
+  var log = createLog(filename)
+  var offset
 
   function assertStream(t, next) {
     return function assertStream (opts, expected) {
@@ -58,6 +60,7 @@ module.exports = function (createLog, cb) {
     return ary.map(function (e) { return e.seq })
   }
   console.log('START')
+
   tape('empty stream', function (t) {
     assertStream(t, next)({}, [])
     function next () { t.end() }
@@ -68,7 +71,14 @@ module.exports = function (createLog, cb) {
     t.end()
   })
 
-  tape('stream with 3 items', function (t) {
+  tape('append 1', function (t) {
+    log.append('a', function (err, seq) {
+      t.ok(seq > -1, 'offset is greater than -1')
+      t.end()
+    })
+  })
+
+  tape('append 3', function (t) {
     //t.plan(19)
 
     //since it's a batch, update at once.
@@ -78,58 +88,74 @@ module.exports = function (createLog, cb) {
       _since = v
     }, false)
 
-    log.append(['a','b','c'], function (err, seq) {
+    log.append(['b','c'], function (err, seq) {
       if(err) throw err
+      console.log('append', err, seq, log.since.value)
       t.equal(log.since.value, seq)
       t.equal(_since, log.since.value)
+      offset = seq
 
-      pull(
-        log.stream({gte: 0, lte: seq, seqs:true, values:true}),
-        pull.collect(function (err, ary) {
-          if(err) throw err
-          var expected = ary
-
-          var _0 = expected[0].seq
-          var _2 = expected[2].seq
-          var n = 16
-
-          console.log('--------------------------------------')
-          var test = assertStream(t, next)
-
-          test({seqs: false}, values(expected))
-          test({gt: _0, seqs: false}, values(expected.slice(1)))
-          test({gt: _0, reverse: true, seqs: false}, values(expected.slice(1)).reverse())
-          test({gte: _0, seqs:false}, values(expected))
-          test({lt: _2, seqs: false}, values(expected.slice(0, 2)))
-
-          test( {},                     (expected))
-          test({gt: _0},                (expected.slice(1)))
-          test({gt: _0, reverse: true}, (expected.slice(1)).reverse())
-          test({gte: _0},               (expected))
-          test({lt: _2},                (expected.slice(0, 2)))
-
-          test({values: false},                       seqs(expected))
-          test({gt: _0, values: false},                seqs(expected.slice(1)))
-          test({gt: _0, reverse: true, values: false}, seqs(expected.slice(1)).reverse())
-          test({gte: _0, values: false},               seqs(expected))
-          test({lt: _2, values: false},                seqs(expected.slice(0, 2)))
-
-          test({gt: _2}, [])
-
-          function next () {
-            if(--n) return
+      log.get(seq, function (err, value, _, __, offset) {
+        console.log("PREV OFFSET", value, offset)
+        t.equal(value, 'c')
+        pull(
+          log.stream(),
+          pull.collect(function (err, values) {
+            t.equal(values.pop().seq, seq)
             t.end()
-          }
-
-        })
-      )
+          })
+        )
+      })
     })
+  })
+
+  tape('stream', function (t) {
+    pull(
+      log.stream({gte: 0, lte: offset, seqs:true, values:true}),
+      pull.collect(function (err, ary) {
+        if(err) throw err
+        var expected = ary
+
+        var _0 = expected[0].seq
+        var _2 = expected[2].seq
+        var n = 16
+        console.log('--------------------------------------')
+        var test = assertStream(t, next)
+
+        test({seqs: false}, values(expected))
+        test({gt: _0, seqs: false}, values(expected.slice(1)))
+        test({gt: _0, reverse: true, seqs: false}, values(expected.slice(1)).reverse())
+        test({gte: _0, seqs:false}, values(expected))
+        test({lt: _2, seqs: false}, values(expected.slice(0, 2)))
+
+        test( {},                     (expected))
+        test({gt: _0},                (expected.slice(1)))
+        test({gt: _0, reverse: true}, (expected.slice(1)).reverse())
+        test({gte: _0},               (expected))
+        test({lt: _2},                (expected.slice(0, 2)))
+
+        test({values: false},                       seqs(expected))
+        test({gt: _0, values: false},                seqs(expected.slice(1)))
+        test({gt: _0, reverse: true, values: false}, seqs(expected.slice(1)).reverse())
+        test({gte: _0, values: false},               seqs(expected))
+        test({lt: _2, values: false},                seqs(expected.slice(0, 2)))
+
+        test({gt: _2}, [])
+
+        function next () {
+          if(--n) return
+          t.end()
+        }
+
+      })
+    )
   })
 
   tape('get', function (t) {
     pull(
       log.stream({seqs: true, values: false}),
       pull.asyncMap(function (seq, cb) {
+        console.log(seq)
         log.get(seq, cb)
       }),
       pull.collect(function (err, ary) {
@@ -159,6 +185,9 @@ module.exports = function (createLog, cb) {
         ended ++
         if(ended > 1) throw new Error('ended twice')
         log.append('e', function (err, _seq) {
+          offset = _seq
+          console.log("APPEND - e", _seq)
+          t.equal(log.since.value, _seq)
           t.equal(ended, 1, 'ended only once')
           //check that it did not read the 'e'
           t.deepEqual(seen, ['a', 'b', 'c', 'd'])
@@ -174,19 +203,42 @@ module.exports = function (createLog, cb) {
       })
     )
 
-    tape('done', function (t) {
-      console.log("DONE")
-      //t.end()
-      cb()
-    })
-
     var last = log.since.value
     log.append(['d'], function (err, seq) {
       if(err) throw err
-      console.log('d', seq)
+      console.log('APPEND - d', seq)
       t.ok(seq > last)
       seq = last
+      offset = seq
     })
+
+
+  })
+
+  tape('close', function (t) {
+    log.close(function () {
+      console.log('drained', log.since.value)
+//      console.log(require('fs').statSync(filename))
+      t.end()
+    })
+//      t.end()
+  })
+
+  tape('reopen', function (t) {
+    var log = createLog(filename)
+    log.since.once(function (v) {
+      t.equal(v, offset, 'same offset after reload')
+      log.get(offset, function (err, value) {
+        t.equal(value, 'e')
+        t.end()
+      })
+    })
+  })
+
+  tape('done', function (t) {
+    console.log("DONE")
+    t.end()
+    cb()
   })
 
 }
